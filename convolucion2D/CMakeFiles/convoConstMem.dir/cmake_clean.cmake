149a3f1213710ba898074db9fff28e64d84d11e5
Karen <dkream@utp.edu.co>
2018-06-06 11:54:11 -0500

    Basura

---
+++ convolucion2D/CMakeFiles/convoConstMem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/convoConstMem.dir/convoConstMem_generated_convoConstMem.cu.o"
+  "convoConstMem.pdb"
+  "convoConstMem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/convoConstMem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
